e8e146682ec3b8b4f98438aad0e386ad2f6c7bcb
Austin Carman <abcarman12@gmail.com>
2021-06-03 16:37:56 -0600

    added more

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/', (req,res) => {
-    res.send(`<h1>Hello There</h1>`)
+    res.send(
+        `<h1>Hello, Welcome To My Deployed Page</h1>
+        <h2>.</h2>
+        <h3>..</h3>
+        <h4>...</h4>
+        <h5>....</h5>
+        <h6>.....</h6>
+        <h7>......</h7>
+        <p>||</p>
+        <p>||</p>
+    `)
 })
 
 server.use('/api', (req, res) => {
