8768715590d32c4604fd0862a5561de9dc7b93ab
Austin Carman <abcarman12@gmail.com>
2021-06-03 16:40:54 -0600

    xmas tree

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ server.use(cors());
 server.use('/', (req,res) => {
     res.send(
         `<h1>Hello, Welcome To My Deployed Page</h1>
-        <h2>.</h2>
-        <h3>..</h3>
-        <h4>...</h4>
-        <h5>....</h5>
-        <h6>.....</h6>
-        <h7>......</h7>
-        <p>||</p>
-        <p>||</p>
+        <h2>     .</h2>
+        <h3>    . .</h3>
+        <h4>   . . .</h4>
+        <h5>  . . . .</h5>
+        <h6> . . . . .</h6>
+        <h7>. . . . . .</h7>
+        <p>     ||</p>
+        <p>     ||</p>
     `)
 })
 
