e1e19b973af9d4a9ef94df8122c6ca0d2a62214b
Austin Carman <abcarman12@gmail.com>
2021-06-03 14:29:42 -0600

    wip

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 
 server.use(express.json());
+
+server.use(cors());
 
 server.use((req, res) => {
     res.status(404).json({
